54cc903101a93ccb5625c4784e8fce43b3da29d9
Kaju2579 <Kajalman2606@gmail.com>
2024-07-02 14:42:58 +0530

    yess

---
+++ miracle/src/in/co/javaPrograms/Mathematics.java
@@ -0,0 +1,10 @@
+package in.co.javaPrograms;
+
+public class Mathematics {
+	public static void main(String args []) {
+		
+  String name = "kajal mandloi";
+  System.out.println(name);
+
+	}
+}
---
+++ miracle/src/in/co/javaPrograms/Miracle.java
@@ -0,0 +1,8 @@
+package in.co.javaPrograms;
+
+public class Miracle {
+	public static void main(String []args) {
+		System.out.println(" hello kajal");
+	}
+
+}
